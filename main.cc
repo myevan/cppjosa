757c4abe85a09ef2e20a8a87ff90c1dc24624012
jaru <myevan80@gmail.com>
2013-05-18 03:25:59 +0900

    ! 예제 소스에 사용된 문구가 UTF8 임을 명시적으로 알림

--- main.cc
+++ main.cc
@@ -3,7 +3,7 @@
 int main()
 {
     std::wstring dstText;
-    std::wstring srcText = Myevan::Korean::U("아노아(은)는 자루(와)과 오리(을)를 칭송하고 절(으)로 들어갔습니다.");
+    std::wstring srcText = System::Text::UTF8Encoding.GetString("아노아(은)는 자루(와)과 오리(을)를 칭송하고 절(으)로 들어갔습니다.");
     Myevan::Korean::ReplaceJosa(srcText, dstText);
 
     std::vector<char> buf;
